5134858c67ced63aa3b63a7f85cc43425dea9784
meixuesong <meixuesong@gmail.com>
2017-11-19 22:47:26 +0800

    init commit, ready for refactor

---
+++ test/refactor/adapter/xml/XMLBuilderTest.java
@@ -0,0 +1,8 @@
+package refactor.adapter.xml;
+
+public class XMLBuilderTest extends AbstractBuilderTest {
+
+	protected OutputBuilder createOutputBuilder(String rootName) {
+		return new XMLBuilder(rootName);
+	}
+}
